4cbfea823108837360229c997b5dcbbf5c23e9de
sxgsgnh <sxgsgnh@sina.com>
2018-10-22 06:06:32 +0800

    update

--- ssupdate.py
+++ ssupdate.py
@@ -2,17 +2,26 @@
 # coding: utf-8
 import sys
 import re
+import os
+import subprocess
 import json
 import copy
 import getopt
 import base64
 import urllib.request
 
-test = b"ssr://Y24wMi5oa2JuLm1sOjQ1MzU3Om9yaWdpbjphZXMtMjU2LWNmYjpwbGFpbjpiMWhtU1hRMS8_b2Jmc3BhcmFtPSZwcm90b3BhcmFtPSZyZW1hcmtzPU1TRG10N0hsbkxQcG1MX3BoNHprdXBIa3VLM292YXdnTlRBd1RXSndjeUF5NVlDTklPaW5odW1pa2VXa3AtYTFnZW1Iai1pS2d1ZUN1U0FnNXJpNDVvaVA1NVNvNW9pMzZLLTM1TDJfNTVTbzVMaUw2WjJpNmFhWjVyaXY1TGlUNTdxX0lDMGc2YWFaNXJpdlNFdFVNREVnTVVkaWNITWdNQzQ0NVlDTklFNWxkR1pzYVhnZ1NFSlBJRlJXUWcmZ3JvdXA9YzNOeVkyeHZkV1E"
-ssrsite = 'https://www.coit.ml/link/gvEUn6gIN6IKOX7I?mu=0'
 regRssaddr = '^http[s]{0,1}:\/\/\S*=\d{1}$'
 regssrbase = '^ssr:\/\/\S*'
 regssraddr = '^\S*:\d{0,5}:\S*:\S*:.*'
+
+confpath = '/etc/shadowsocksr/rss/'
+ssrpath = '/etc/shadowsocksr/'
+version = 'ssrupdate version 0.1'
+
+
+def usage():
+    print("this a usage")
+    pass
 
 
 def base_adjust(basecode):
@@ -26,8 +35,9 @@ def base_adjust(basecode):
 
 
 def parser_ssr_list(Rss):
+    print(Rss)
     try:
-        date = urllib.request.urlopen(ssrsite)
+        date = urllib.request.urlopen(Rss)
         ss = date.read()
     except InterruptedError:
         print("recvire data error")
@@ -37,11 +47,13 @@ def parser_ssr_list(Rss):
 
 
 def load_default_config():
-    try:
-        ss = json.load(open("/etc/shadowsocksr/config.json", 'r'))
-    except IOError:
-        print("default confige not exist , plase check shadowsocksr is installed")
-    return ss
+    if not os.path.exists(ssrpath+'config_def.json'):
+        conf = json.load(open(ssrpath+'config.json', 'r'))
+        json.dump(conf, open(ssrpath+'config_def.json', 'w'), ensure_ascii=False, indent=4)
+    else:
+        conf = json.load(open(ssrpath+'config_def.json', 'r'))
+    return conf
+
 
 def parser_ssr_addr(jsondata, ssraddr):
     if (re.match(regssrbase, str(ssraddr, encoding='utf-8'))):
@@ -51,39 +63,180 @@ def parser_ssr_addr(jsondata, ssraddr):
             data = data.split(b'/?')
             data1 = data[0].split(b':')
             jsondata['server'] = str(data1[0], encoding='utf-8')
-            jsondata['server_port'] = str(data1[1], encoding='utf-8')
+            jsondata['server_port'] = int(data1[1])
             jsondata['protocol'] = str(data1[2], encoding='utf-8')
             jsondata['method'] = str(data1[3], encoding='utf-8')
             jsondata['obfs'] = str(data1[4], encoding='utf-8')
-            jsondata['password'] = str(base64.b64decode(base_adjust(data1[5])), encoding='utf-8')
+            jsondata['password'] = str(base64.b64decode(base_adjust(data1[5])),\
+                                       encoding='utf-8')
             data2 = data[1].split(b'&')
+
             tmp = data2[0].split(b'=')
             if(len(tmp[1]) != 0):
                 jsondata['obfs_param'] = str(base64.b64decode(base_adjust(tmp[1])), encoding='utf-8')
+
             tmp = data2[1].split(b'=')
             if(len(tmp[1]) != 0):
                 jsondata['protocol_param'] = str(base64.b64decode(base_adjust(tmp[1])), encoding='utf-8')
+
             tmp = data2[2].split(b'=')
             if(len(tmp) != 0):
                 jsondata['remarks'] = str(base64.b64decode(base_adjust(tmp[1])), encoding='utf-8')
+
             tmp = data2[3].split(b'=')
             if(len(tmp) != 0):
                 jsondata['group'] = str(base64.b64decode(base_adjust(tmp[1])), encoding='utf-8')
-            return jsondata
+                pass
+        return jsondata
+
+
+def write_ssr_config_file(ssr):
+    ssrdata = load_default_config()
+
+    ssrdata = parser_ssr_addr(ssrdata, ssr)
+
+    group = 'defaults'
+    if 'group' in ssrdata:
+        group = ssrdata['group']
+        del ssrdata['group']
+        pass
+
+    ssrgrp = {}
+    if os.path.exists(confpath+group+'.json'):
+        ssrgrp = json.load(open(confpath+group+'.json', 'r'))
+        pass
+
+    ssrgrp[str(len(ssrgrp)+1)] = ssrdata
+
+    json.dump(ssrgrp, open(confpath+group+'.json', 'w'), ensure_ascii=False, indent=4)
+    return True
+
+
+def update_rss(ssrsite):
+    data = parser_ssr_list(ssrsite)[2:]
+    for x in range(len(data)-1):
+        write_ssr_config_file(data[x])
+        pass
+    print('Completed!')
     pass
 
-print(parser_ssr_addr(load_default_config(), test))
+
+def ping_test(server):
+    cmd = 'ping -W 1 -c 2 ' + server
+    res = subprocess.getstatusoutput(cmd)
+    if (res[0] == 0):
+        res = re.findall("time=.*ms", res[1])
+        return res[1]
+    return "faile"
+
+
+def server_speed(group, cid):
+    if os.path.exists(confpath + group + '.json'):
+        data = json.load(open(confpath+group+'.json', 'r'))
+        s = ping_test(data[cid]['server'])
+        remark = ''
+        if 'remarks' in data[cid]:
+            remark = data[cid]['remarks']
+        print('id:'+cid+' server://'+data[cid]['server']+' '+s+' '+remark)
+        return True
+
+
+def server_speed_group(group):
+    pass
+
+
+def group_list():
+    grp = os.listdir(confpath)
+    for e in grp:
+        d = json.load(open(confpath+e, 'r'))
+        print('[Group: '+e.split('.')[0]+'] [Range: 1~'+str(len(d))+']')
+    pass
+
+
+def show_group(grp):
+    if os.path.exists(confpath+grp+'.json'):
+        data = json.load(open(confpath+grp+'.json', 'r'))
+        for v in data:
+            remark = ''
+            if 'remarks' in data[v]:
+                remark = data[v]['remarks']
+                pass
+            print('id:'+v+' '+data[v]['server']+' '+remark)
+            pass
+        pass
+    else:
+        print('group is not exists')
+    pass
+
+
+def delete_group(grp):
+    if os.path.exists(confpath+grp+'.json'):
+        os.remove(confpath+grp+'.json')
+        pass
+    pass
+
+
+def switch_config(grp, id):
+    print(grp)
+    if os.path.exists(confpath+grp+'.json'):
+        data = json.load(open(confpath+grp+'.json', 'r'))
+        if id in data:
+            new_config = copy.copy(data[id])
+            del new_config['remarks']
+            json.dump(new_config, open(ssrpath+'config.json', 'w'), ensure_ascii=False, indent=4)
+            ret = subprocess.getstatusoutput('systemctl is-active shadowsocksr@config')
+            if ret[1] == 'active':
+                os.system('systemctl restart shadowsocksr@config')
+                print('restart shadowsocksr@config')
+                pass
+            print('completed')
+            pass
+        else:
+            print('id is not exists')
+        pass
+    else:
+        print('group is not exists')
+    pass
+
 
 def main(argv):
+
+    if not os.path.exists(confpath):
+        os.mkdir(confpath)
+
     try:
-        arg = getopt.getopt(argv[1:],"hv",["help","version"])
+        opt, arg = getopt.getopt(argv[1:], "hvgu:t:s:d:c:",\
+                                 ["help", "version", "rss=", "ssr="])
     except IOError:
         print("error")
         pass
-    print(arg)
+    for n, v in opt:
+        if n in ['-v', '-version']:
+            print(version)
+            pass
+        elif n in ('-h', '--help'):
+            usage()
+            pass
+        elif n == '-t':
+            s = v.split('/')
+            server_speed(s[0], s[1])
+        elif (n == '-g'):
+            group_list()
+        elif n == '--rss':
+            update_rss(v)
+        elif n == '-s':
+            show_group(v)
+        elif n == '-c':
+            s = v.split('/')
+            switch_config(s[0], s[1])
+        pass
     pass
 
 
 if __name__ == '__main__':
-    #main(sys.argv)
+    if (os.name == 'posix') and os.path.isdir('/etc/shadowsocksr'):
+        main(sys.argv)
+        pass
+    else:
+        print('not support os platrom or ssr not installed')
     pass
